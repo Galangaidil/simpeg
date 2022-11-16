033a4aa750eec4160eb82517dc133e4d3b6c2068
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2022-11-16 12:21:57 +0700

    Add Auth API

---
+++ app/Http/Controllers/ApiAuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class ApiAuthController extends Controller
+{
+    use HttpResponses;
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $this->success([
+            'token' => $user->createToken($request->device_name)->plainTextToken,
+        ], 'Login success');
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->success([
+            'message' => 'Logout success dan token telah dihapus'
+        ]);
+    }
+}
